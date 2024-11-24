20237fa31da19c366ea27e87a6a239e0a96bd277
RegineGoh <130035490+RegineGoh@users.noreply.github.com>
2024-11-24 14:03:30 +0800

    search part build(ver2)

--- code/search.py
+++ code/search.py
@@ -1,14 +1,24 @@
 from tkinter import *
+from tkinter import ttk
 import database
 # getAllData()，回傳一個list包含所有消費紀錄的(Year, Month, Date, Time, Name, Price)
 # getAllTag(Tag)，以list回傳所有使用到此Tag名稱(string)的資料
 
+year=-1
+month=0
+date=0
+tag=-1
+name=""
+valueOrMin=-1
+max=-1
+
 data=getAllData()
-allYear=[]
 
 def getYear():
+    allYear=[]
     for row in data:
         allYear.append(row[0])
+    return allYear
 
 def searchDate(year, month, date):
     newData=[]
@@ -74,6 +84,30 @@ def showResult():
 
     reslutWindow.mainloop()
 
+def search():
+    if(yearBox.current()>=0):
+        year=yearBox.get()
+        month=monthBox.get(1.0, 'end-1c')
+        date=dateBox.get(1.0, 'end-1c')
+        if(date!=0):
+            searchDate(year, month, date)
+        else:
+            searchMonth(year, month)
+    if(tagBox.current()>=0):
+        tag=tagBox.get()
+        searchTag()
+    name=itemBox.get(1.0, 'end-1c')
+    if(name!=0):
+        searchItem(name)
+    valueOrMin=minBox.get(1.0, 'end-1c')
+    max=maxBox.get(1.0, 'end-1c')
+    if(valueOrMin!=-1):
+        if(max==-1):
+            searchAmount(valueOrMin)
+        else:
+            searchRange(valueOrMin, max)
+    showResult()
+    
 window=Tk()
 window.title("Search")
 
@@ -81,5 +115,33 @@ Label(window, text="Date:").grid(row=0, column=0, padx=10, pady=5, sticky="w")
 Label(window, text="Tag:").grid(row=3, column=0, padx=10, pady=5, sticky="w")
 Label(window, text="Item:").grid(row=4, column=0, padx=10, pady=5, sticky="w")
 Label(window, text="Price:").grid(row=5, column=0, padx=10, pady=5, sticky="w")
+
+Label(window, text="Year:").grid(row=0, column=1, padx=10, pady=5, sticky="w")
+Label(window, text="Month:").grid(row=1, column=1, padx=10, pady=5, sticky="w")
+Label(window, text="Date:").grid(row=2, column=1, padx=10, pady=5, sticky="w")
+
+yearBox = ttk.Combobox(window, values=getYear(), width=5)
+yearBox.grid(row=0, column=2, padx=10, pady=5, sticky="w")
+
+monthBox = Text(window, height=1, width=3)
+monthBox.grid(row=1, column=2, padx=10, pady=5, sticky="w")
+
+dateBox = Text(window, height=1, width=3)
+dateBox.grid(row=2, column=2, padx=10, pady=5, sticky="w")
+
+tagBox = ttk.Combobox(window, values=getAllTag())
+tagBox.grid(row=3, column=2, padx=10, pady=5, sticky="w")
+
+itemBox = Text(window, height=1, width=21)
+itemBox.grid(row=4, column=1, columnspan=2, padx=10, pady=5, sticky="w")
+
+minBox = Text(window, height=1, width=8)
+minBox.grid(row=5, column=1, padx=10, pady=5, sticky="w")
+Label(window, text="~").grid(row=5, column=2, padx=1, pady=2, sticky="w")
+maxBox = Text(window, height=1, width=8)
+maxBox.grid(row=5, column=2, padx=10, pady=5, sticky="e")
+
+btn = Button(window, text='Search', command=search)
+btn.grid(row=6, column=2, padx=10, pady=5, sticky="e")
 
 window.mainloop()
